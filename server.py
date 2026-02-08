03130464df1522b1ef2a2ab45be7ce137642338e
amxncore <amanmuttilara@gmail.com>
2026-02-09 00:03:22 +0530

    feat: notes api with GET and POST from scratch using sockets

--- server.py
+++ server.py
@@ -3,25 +3,45 @@ import socket
 HOST = "127.0.0.1"
 PORT = 9090
 
+notes = []
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
-server.listen(1)
+server.listen(5)
 
 print(f"server running on http://{HOST}:{PORT}")
 
 while True:
     client, addr = server.accept()
-    request = client.recv(1024).decode()
+    request = client.recv(4096).decode()
 
-    response_body = "hi batman"
+    print("----- REQUEST -----")
+    print(request)
 
-    response = (
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/plain\r\n"
-        f"Content-Length: {len(response_body)}\r\n"
-        "\r\n"
-        + response_body
-    )
+    try:
+        if request.startswith("GET /notes"):
+            response_body = str(notes)
 
-    client.sendall(response.encode())
-    client.close()
+        elif request.startswith("POST /notes"):
+            body = request.split("\r\n\r\n", 1)[1]
+            notes.append(body)
+            response_body = '{"message":"note saved"}'
+
+        else:
+            response_body = "hi batman"
+
+        response = (
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: application/json\r\n"
+            f"Content-Length: {len(response_body)}\r\n"
+            "\r\n"
+            + response_body
+        )
+
+        client.sendall(response.encode())
+
+    except Exception as e:
+        print("ERROR:", e)
+
+    finally:
+        client.close()
